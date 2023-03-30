476b67aad9efae27a9ae41f60fa5071e692e3b5a
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-30 15:02:13 -0300

    req 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import PlanetCard from './PlanetCard';
 import Title from './Title';
+import Planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <PlanetCard />
+        <ul>
+          <li>
+            {
+              Planets.map(({ name, image }) => (
+                <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+              ))
+            }
+          </li>
+        </ul>
       </div>
     );
   }
