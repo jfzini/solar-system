2cb170df7c0bc1c22ee59a283f7afe3b10b6fe0d
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-30 15:05:27 -0300

    req 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Missions />
       </>
     );
   }
