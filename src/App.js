457b4007a18c8484b8fb0cf0093ca1e01ce9b3f5
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-30 14:37:32 -0300

    add previous reqs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
