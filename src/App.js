e34d9dd49cc38c33f5066b202ff0ee719e9a2caf
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-01 11:36:55 -0300

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
+import Footer from './components/Footer';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        Developed by João Felipe Zini
+        <span className="material-symbols-outlined">
+          copyright
+        </span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
